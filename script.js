a8f65ca0e993e1b06ef8997d9656507e2c846679
FrancoCarra <104314979+FrancoCarra@users.noreply.github.com>
2022-07-27 15:01:09 +0200

    Presentacion 2

--- script.js
+++ script.js
@@ -1,82 +1,95 @@
-//creo los servicios
-let servicio = [
-    {
-        nombre: 'Lamparita S.R.L',
-        precio: 0,
-        servicio: 'Luz',
-        imagen: 'img/Luz comp.jpeg',
-},
-{
-    nombre: 'Aguas Turbias',
-    precio: 0,
-    servicio: 'agua',
-    imagen: 'img/Agua comp.jpeg',
-},
-{
-    nombre: 'Gases Naturales',
-    precio: 0,
-    servicio: 'Gas',
-    imagen: 'img/Gas comp.jpeg',
+class Servicio {
+    constructor (nombre, precio, lugar, ){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.lugar = lugar;
+    }
 }
-]
-//agrego los servicios de manera interactiva al html
+const servicios = []
 
-let Sv = document.getElementById('miServicio')
-    for (const x of servicio) {
-        Sv.innerHTML += `
-                    <div id="formServicio" class="card main-div col m-1" style="width: 18rem;">
-                        <img src="${x.imagen}" class="card-img-top" alt="#">
-                            <div class="card-body">
-                                <h5 id="nombreServicio" class="card-title">${x.nombre}</h5>
-                                <p class="card-text">${x.servicio}</p>
-                                <input type="number" class="form-control" id="precioServicio" name="precio">
-                                <button type="submit" id="botonServicio" class="boton"> Agregar al Carrito </button>
-                            </div>
-                    </div>
-    `
-}
-//guardo en local storage
+const idFormulario = document.getElementById('formulario');
 
-if(localStorage.getItem('servicio')) {
-    tareas = JSON.parse(localStorage.getItem('servicio'))
-} else {
-    localStorage.setItem('servicio', JSON.stringify(servicio))
-}
+idFormulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const precio = document.getElementById('precio').value;
+    const lugar = document.getElementById('lugar').value;
 
-const formServicio = document.getElementById("formServicio")
-const nombreServicio = document.getElementById("nombreServicio")
-const precioServicio = document.getElementById("precioServicio")
-const botonServicio = document.getElementById("botonServicio")
+    const servicio = new Servicio (nombre, precio, lugar,);
+    servicios.push(servicio);
 
-//agregamos carrito, aca me hice un lio jaja
+    localStorage.setItem('Servicio', JSON.stringify(servicios));
+    idFormulario.reset();
 
-function AgregarServicio(){
-    let x = {}
-    x.valor = document.querySelector("#precioServicio").value;
-    console.log(x.valor)
-}
-botonServicio.addEventListener('click', () => {
-    let arrayStorage = JSON.parse(localStorage.getItem('servicio'))
-    divServicios.innerHTML = ""
-    arrayStorage.forEach((servicio, indice) => {
-        divServicios.innerHTML += `
-        <div class="card border-dark mb-3" id="tarea${indice}" style="max-width: 20rem; margin:4px;">
-            <div class="card-header"><h2>${servicio.nombre}</h2></div>
-            <div class="card-text"><h3>${servicio.precio}</h3></div>
-            <div class="card-body">
-                <p class="card-title">${servicio.servicio}</p>
-                <button class="btn btn-danger">Eliminar Servicio</button>
-            </div>
-        </div>
-        `
-    });
+    mostrarInfo(servicio);
+    mostrarTot(servicio);
 })
+const servicio = new Servicio (nombre, precio, lugar,);
+const resultado = document.getElementById('infoServicios');
 
-const editButtons = document.querySelectorAll(".boton");
-editButtons.forEach(botonServicio => botonServicio.addEventListener('click',AgregarServicio, () => confirm));
+const mostrarInfo = (servicio) => {
+    let aux = '';
+    aux += `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h2 class="card-title">${servicio.nombre}</h2>
+            <p class="card-text">su total a pagar es: ${servicio.precio}</p>
+            <p class="card-text">en la localidad de: ${servicio.lugar}</p>
+        </div>
+    </div>`;
+    resultado.innerHTML = aux;
+    }
+
+opciones = document.formularioMeses.mes;
+elegido = opciones.value;
+tot = servicio.precio;
+
+    function mostrarTot (servicio) {
+        tot = servicio.precio;
+            if(isNaN(tot)){
+                console.log(tot)
+            }
+            else{
+                console.log(tot)
+            }
+        }
+
+    function cambia() {
+        opciones = document.formularioMeses.mes;
+        for (i=0;i<opciones.length;i++) {
+           if (opciones[i].checked == true) {
+              elegido = opciones[i].value
+              }
+           }
+       switch (elegido) {
+         case "1":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break;
+         case "2":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break; 
+         case "3":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break;
+         case "4":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break;
+         case "5":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break;
+         case "6":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break;
+         case "7":
+            console.log(`Su total a pagar es: ${tot * opciones.value}$`)
+         break;
+         default: 
+         alert('ingrese un valor valido');
+         break;
+         } 
+       }
 
 //Boton modo oscuro
-
 botonModoNoche.addEventListener("click", () => {
     document.body.classList.add('darkMode')
     localStorage.setItem('tema', "oscuro")  
